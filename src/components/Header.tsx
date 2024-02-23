8d03322980677492fc10990cf556889f42138c12
LuizinhoGustavo <lggustavo329@gmail.com>
2024-02-22 23:18:54 -0300

    Tasks Finalizadas e Sonner configurado

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,12 @@
 /*import { Moon } from "@phosphor-icons/react";*/
 
+import { toast } from "sonner"
+
+function createTask() {
+
+    toast.success("Tarefa criada com sucesso!")
+}
+
 export function Header() {
     return (
         <div className="my-10 mx-15x flex-1 grid grid-cols-1">
@@ -8,7 +15,7 @@ export function Header() {
                 {/*<Moon className="h-5 w-5 my-[4px] flex-" />*/}
             </div >
             <div className="w-3/4 mx-auto py-8 flex gap-3">
-                <button className="h-14 w-14 border text-3xl border-black rounded-full hover:bg-gray-100 hover:delay-50">+</button>
+                <button onClick={createTask} className="h-14 w-14 border text-3xl border-black rounded-full hover:bg-gray-100 hover:delay-50">+</button>
                 <input type="text" placeholder="Digite uma nova tarefa" className="border flex-1 border-black rounded-3xl resize-none text-2xl px-4 focus:outline-none"></input>
             </div>
 
