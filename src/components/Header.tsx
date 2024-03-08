546bd14d6f1d92133cbe33e33c52fdc156ca41e7
LuizinhoGustavo <lggustavo329@gmail.com>
2024-03-07 22:01:52 -0300

    Finalização do Design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 
 import { toast } from "sonner"
 
-function createTask() {
+function createNewTask() {
 
     toast.success("Tarefa criada com sucesso!")
 }
@@ -15,12 +15,11 @@ export function Header() {
                 {/*<Moon className="h-5 w-5 my-[4px] flex-" />*/}
             </div >
             <div className="w-3/4 mx-auto py-8 flex gap-3">
-                <button onClick={createTask} className="h-14 w-14 border text-3xl border-black rounded-full hover:bg-gray-100 hover:delay-50">+</button>
-                <input type="text" placeholder="Digite uma nova tarefa" className="border flex-1 border-black rounded-3xl resize-none text-2xl px-4 focus:outline-none"></input>
+                <button onClick={createNewTask} className="h-14 w-14 border text-3xl border-black rounded-full hover:bg-gray-100 hover:delay-50">+</button>
+                <input type="text" id="task" placeholder="Digite uma nova tarefa" className="border flex-1 border-black rounded-3xl resize-none text-2xl px-4 focus:outline-none"></input>
             </div>
 
             <div className="border border-black w-3/4 mx-auto my-4" />
-
 
         </div >
     )
--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Header } from './components/Header'
 import './index.css'
-import { TODO } from './components/TODO'
+import { Header } from './components/Header'
+import { Task } from './components/Task'
 import { Toaster } from 'sonner'
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Header />
-    <TODO />
+    <Task content='AAAA' />
     <Toaster richColors />
   </React.StrictMode>
 )
